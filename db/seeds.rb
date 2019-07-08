a3892f64b028085a48731616b65df61a61998bad
yonureker <yonureker@gmail.com>
2019-07-08 11:19:02 -0700

    commit for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'faker'
+
 User.destroy_all
 Task.destroy_all
 
