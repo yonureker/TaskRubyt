154729f465010da12408ba3469ef29c484843658
yonureker <yonureker@gmail.com>
2019-07-12 09:08:26 -0700

    commit for heroku

--- db/migrate/20190711225030_drop_active_storage_attachments_table.rb
+++
@@ -1,5 +0,0 @@
-# class DropActiveStorageAttachmentsTable < ActiveRecord::Migration[5.2]
-#   def change
-#     drop_table :active_storage_attachments
-#   end
-# end
--- db/migrate/20190711225159_drop_active_storage_blobs_table.rb
+++
@@ -1,5 +0,0 @@
-# class DropActiveStorageBlobsTable < ActiveRecord::Migration[5.2]
-#   def change
-#     drop_table :active_storage_blobs
-#   end
-# end
