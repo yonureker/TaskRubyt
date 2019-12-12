521aa9ffa6214f0f478fbe94c708b14a68e43b89
yonureker <yonureker@gmail.com>
2019-12-12 12:57:01 -0800

    update signup form styling

--- frontend/components/session_form/signup_form.jsx
+++ frontend/components/session_form/signup_form.jsx
@@ -44,38 +44,38 @@ class SignupForm extends React.Component {
           <br/>
           <div className="signup-form">
             <div className="signup-form-logo">
-            <img src="https://i.ibb.co/G9Q7mbk/diamond2.png" className="diamond" border="0" height="30" width="27" /><strong className="text">Task<span className="greeny">Rubyt</span></strong>
+            <img src="https://i.ibb.co/G9Q7mbk/diamond2.png" className="diamond" border="0" height="30" width="27" /><strong class="text">Task<span className="greeny">Rubyt</span></strong>
             </div>
-            <br/><div>
+            <br/>
             <label>Email Address</label>
               <input type="text"
                 value={this.state.email}
                 onChange={this.update('email')}
                 className="signup-input"
               />
-              <div className="errors">{this.handleErrors("Email")}</div></div>
-              <label>Password</label>
+              <div className="errors">{this.handleErrors("Email")}</div>
+              <br/><label>Password</label>
               <input type="password"
                 value={this.state.password}
                 onChange={this.update('password')}
                 className="signup-input"
               />
               <div className="errors">{this.handleErrors("Password")}</div>
-              <label>First Name</label>
+              <br/><label>First Name</label>
               <input type="text"
                 value={this.state.first_name}
                 onChange={this.update('first_name')}
                 className="signup-input"
               />
               <div className="errors">{this.handleErrors("First")}</div>
-              <label>Last Name</label>
+              <br/><label>Last Name</label>
               <input type="text"
                 value={this.state.last_name}
                 onChange={this.update('last_name')}
                 className="signup-input"
               />
               <div className="errors">{this.handleErrors("Last")}</div>
-              <label>Zip Code</label>
+              <br/><label>Zip Code</label>
               <input type="number"
                 value={this.state.zip_code}
                 onChange={this.update('zip_code')}
