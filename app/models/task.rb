c9faabaf4710d7d109bcdb442e1ab78a70432e44
yonureker <yonureker@gmail.com>
2019-12-13 14:30:38 -0800

    update task

--- app/models/task.rb
+++ app/models/task.rb
@@ -36,8 +36,8 @@ class Task < ApplicationRecord
              foreign_key: :tasker_id,
              class_name: :User
 
-  belongs_to :category,
-          primary_key: :id,
-          foreign_key: :category_id,
-          class_name: :Category
+  # belongs_to :category,
+  #         primary_key: :id,
+  #         foreign_key: :category_id,
+  #         class_name: :Category
 end
